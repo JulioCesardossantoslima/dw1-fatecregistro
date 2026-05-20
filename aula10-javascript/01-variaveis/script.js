6bea254cb2d47308c65ac17dd549277f3d5e4a30
DSM1 <fatec-dsm1@academico.intra>
2026-05-19 22:22:35 -0300

    inicio java script

---
+++ aula10-javascript/01-variaveis/script.js
@@ -0,0 +1,20 @@
+// JavaScript Externo
+// VARIÁVEIS
+/* variaveis podem ser declaradas de 3 formas:
+var, let e const */
+// Evitar usar o VAR, pode não ser muito seguro
+// LET: utilize quando for necessário reatribuir o valor da variavel
+// CONST: utilize quando não precise reatribuir o valor da variavel
+var nome = "diego";
+var numero = 2;
+nome = "joao";
+var nome = "joao";
+// let
+let cidade = "Registro";
+cidade = "Sete barras";
+// não pode usar let para definir "cidade" denovo
+// CONST
+const user = "pedro@email.com";
+user = "pedro@gmail.com"
+// não permitido
+// const user = "pedro@gmail.com" tambem não permitido
